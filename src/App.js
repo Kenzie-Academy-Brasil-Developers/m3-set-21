b024984701d4f4ade5322a843e5ce0c4141d4f47
Gabriel Araújo <gabriel@kenzie.com.br>
2022-02-08 10:46:23 -0300

    feature: pagination

--- src/App.js
+++ src/App.js
@@ -1,15 +1,100 @@
-/** Importações css, bibliotecas, importações de dentro react, componentes */
+import { useState, useEffect } from "react";
 import "./App.css";
-import Card from "./components/Card";
+import Counter from "./components/Counter";
+import axios from "axios";
 
 function App() {
+  const [count, setCount] = useState(34);
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [next, setNext] = useState("");
+  const [error, setError] = useState(false);
+
+  /* useEffect com as 3 situações, pode ser necessário se tudo tiver interligado*/
+  // useEffect(() => {
+  //   if (count === 0) {
+  //     console.log("Montagem"); // "Montagem"
+  //   }
+
+  //   if (count > 0) {
+  //     console.log(count); // 0
+  //     console.log("Atualização");
+  //   }
+
+  //   return () => {
+  //     console.log("Desmontagem");
+  //   };
+  // }, [count]);
+
+  useEffect(() => {
+    // fetch(`https://kenzie-habits.herokuapp.com/groups/?page=${count}`)
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     setNext(response.next);
+    //     setLoading(false);
+    //     setGroups(response.results);
+    //   })
+    //   .catch((err) => {
+    //     setError(true);
+    //     setGroups([]);
+    //   });
+
+    async function fetchGroups() {
+      if (count > 0) {
+        const response = await axios
+          .get(`https://kenzie-habits.herokuapp.com/groups/?page=${count}`)
+          .catch((err) => {
+            setError(true);
+            setGroups([]);
+          });
+
+        setLoading(false);
+        setGroups(response.data.results);
+      }
+    }
+
+    fetchGroups();
+  }, [count]);
+
+  const nextPage = () => {
+    setLoading(true);
+    setCount(count + 1);
+  };
+
+  const previousPage = () => {
+    if (count > 1) {
+      setLoading(true);
+      setCount(count - 1);
+    }
+  };
+
+  if (error) {
+    return (
+      <div>
+        <h2>Ops... Página inválida, deseja retornar ao inicio?</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Card name="Gabriel" />
-        <Card name="Amanda" />
-        <Card name="Wesley" />
-        <Card name="Ivan" />
+        {count <= 10 ? (
+          <Counter count={count} />
+        ) : (
+          <span>A contagem está maior q 10 = {count}</span>
+        )}
+
+        <button onClick={nextPage}>Próxima página</button>
+        <button onClick={previousPage}>Página anterior</button>
+
+        <ul>
+          {loading ? (
+            <h2>Carregando...</h2>
+          ) : (
+            groups.map((group) => <li key={group.id}>{group.name}</li>)
+          )}
+        </ul>
       </header>
     </div>
   );
--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,16 +1,24 @@
 /**Importação, componentes, libs, etc */
 
+import { useEffect } from "react";
+
 // Chamada da função com as props no parametro se existirem
-const Card = ({ name }) => {
+const Counter = ({ count }) => {
   // Logíca acima do return
+
+  useEffect(() => {
+    return () => {
+      console.log("O Counter saiu de tela");
+    };
+  }, []);
 
   return (
     <h1>
       {/* Renderização do jsx e variáveis  */}
-      Olá Mundo! {name}
+      {count}
     </h1>
   );
 };
 
 // exportação para utilizar em outros lugares
-export default Card;
+export default Counter;
